34fb4836f2f397502e63243054098d30064b3d51
klenfes1 <156152210+klenfes1@users.noreply.github.com>
2024-11-14 14:30:16 -0700

    Updating potentiometer to also display through amber LEDs

--- deliverables/4_lab_3/lab/PotentiometerLab_Code/PotentiometerLab_Code.ino
+++ deliverables/4_lab_3/lab/PotentiometerLab_Code/PotentiometerLab_Code.ino
@@ -1,16 +1,43 @@
 #define POTMETERPIN 19
 
+#include <Wire.h>
+#include "Adafruit_MCP23008.h"
+
+Adafruit_MCP23008 mcp_leds;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.swap(3);
   Serial.begin(115200);
+
+  uint8_t pin_id, status;
+
+  status = mcp_leds.begin(0x25);
+
+  for (pin_id = 0; pin_id < 8; pin_id++) {
+    mcp_leds.pinMode(pin_id, OUTPUT);
+    mcp_leds.digitalWrite(pin_id, HIGH);
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("Wow");
 
-  int a = analogRead(POTMETERPIN);
-  
-  Serial.println("Potmeter value: " + String(a));
+  int readValue = analogRead(POTMETERPIN);
+  int leds = 0;
+
+  Serial.println("Potmeter value: " + String(readValue));
+
+  while(readValue > 128) {
+    readValue = readValue - 128; 
+    leds++;
+  }
+
+  for(int i = 0; i < 8; i++){
+    if(i <= leds){
+      mcp_leds.digitalWrite(7-i, LOW);
+    } else {
+      mcp_leds.digitalWrite(7-i, HIGH);
+    }
+  }
 }
