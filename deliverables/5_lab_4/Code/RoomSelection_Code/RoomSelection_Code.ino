9cd8882171d6315f221f0f39769f41b4028f62b9
klenfes1 <156152210+klenfes1@users.noreply.github.com>
2024-11-22 12:30:41 -0700

    Adding switch enabled color-swap

--- deliverables/5_lab_4/Code/RoomSelection_Code/RoomSelection_Code.ino
+++ deliverables/5_lab_4/Code/RoomSelection_Code/RoomSelection_Code.ino
@@ -1,4 +1,7 @@
 #define POTMETERPIN 19
+#define SWITCH_ONE 22
+#define SWITCH_TWO 8
+#define SWITCH_THREE 9
 #include <tinyNeoPixel.h>
 
 tinyNeoPixel pixel_ring = tinyNeoPixel(8, PIN_PC3, NEO_GRB + NEO_KHZ800);
@@ -7,11 +10,15 @@ unsigned long color_grid[3] = {0x00007f,0x007f00,0x7f0000}; // Red, green, and b
 void setup() {
   // put your setup code here, to run once:
   pixel_ring.begin();
+  pinMode(SWITCH_ONE, INPUT);
+  pinMode(SWITCH_TWO, INPUT);
+  pinMode(SWITCH_THREE, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  uint8_t pixel_index = 0, color_index = 0;
+  uint8_t pixel_index = 0;
+  static uint8_t color_index[8] = {0};
 
   int readValue = analogRead(POTMETERPIN);
 
@@ -20,8 +27,14 @@ void loop() {
     pixel_index++;
   }
 
+  int switch_one_value = digitalRead(SWITCH_ONE);
+  int switch_two_value = digitalRead(SWITCH_TWO);
+  int switch_three_value = digitalRead(SWITCH_THREE);
+
+  color_index[pixel_index] = switch_one_value == LOW ? 0 : switch_two_value == LOW ? 1 : switch_three_value == LOW ? 2 : color_index[pixel_index];
+
   pixel_ring.clear();
   pixel_ring.show();
-  pixel_ring.setPixelColor(pixel_index, color_grid[color_index]);
+  pixel_ring.setPixelColor(pixel_index, color_grid[color_index[pixel_index]]);
   pixel_ring.show();
 }
