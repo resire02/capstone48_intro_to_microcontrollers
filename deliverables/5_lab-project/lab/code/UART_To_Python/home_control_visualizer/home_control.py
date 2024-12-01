5d9858c1146e54336eb74f3767894fb3afce4f23
resire02 <howfelix@gmail.com>
2024-12-01 15:10:03 -0700

    added uart connection to visualizer

--- deliverables/5_lab-project/lab/code/UART_To_Python/home_control_visualizer/home_control.py
+++ deliverables/5_lab-project/lab/code/UART_To_Python/home_control_visualizer/home_control.py
@@ -7,28 +7,36 @@ import serial.tools.list_ports_windows as list_ports
 '''
 Config
 '''
+PROGRAM_LOGO = 'home_control_system_logo.ico'
 AVAILABLE_BAUD_RATES = [9600, 115200]
 ROOMS = [
     'Bedroom 1',
     'Master Bedroom',
     'Master Bathroom',
-    'Bedroom 2', 
+    'Bedroom 2',
     'Living Room',
     'Kitchen',
     'Dining Room',
     'Hallway Bathroom',
 ]
+COLOR_RED = '#ff0000'
+COLOR_GREEN = '#00ff00'
+COLOR_BLUE = '#0000ff'
 
 SELECTED_PORT = None
 SELECTED_BAUD_RATE = None
 ROOM_INDEX = 0
 
+###### ARDUINO PORT ROUTING
+
 def set_port(port):
-    global SELECTED_PORT 
+    global SELECTED_PORT
+    print('Selected Port:', port)
     SELECTED_PORT = port
 
 def set_baudrate(rate):
     global SELECTED_BAUD_RATE
+    print('Selected Baud Rate:', rate)
     SELECTED_BAUD_RATE = rate
 
 def arduino_uart_thread():
@@ -42,9 +50,12 @@ def arduino_uart_thread():
         while True:
             if uart.in_waiting:
                 received_data = uart.readline().decode('utf-8').replace('\n', '')
-                print(f'Received: {received_data}')
+                print('Received Data:', received_data)
+                arduino_update_ui(received_data.lower())
     except serial.SerialException:
         create_popup('Error', 'Could not connect to Arduino. Ensure that Arduino IDE is closed\nand that the port name matches the port in device manager and is not being currently used.')
+    finally:
+        UART_CONNECTED = False
 
 def connect_to_ardunio():
     thread = threading.Thread(target=arduino_uart_thread, daemon=True)
@@ -53,10 +64,113 @@ def connect_to_ardunio():
 def list_comports():
     return [(x.name,x.device) for x in list_ports.comports()]
 
+def set_port_func(port):
+    def _inner():
+        set_port(port)
+    return _inner
+
+def set_baudrate_func(baud_rate):
+    def _inner():
+        set_baudrate(baud_rate)
+    return _inner
+
+###### CANVAS DRAW OPERATIONS
+
+def draw_bedroom_1(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x, y, x, y+120, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x, y, x+135, y, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+135, y, x+135, y+60, width=w, fill=color if highlight else None) # right line stick
+    canvas.create_line(x+95, y+60, x+135, y+60,width=w, fill=color if highlight else None) # bottom line stick
+    canvas.create_line(x+95, y+60, x+95, y+120, width=w, fill=color if highlight else None) # right line inner
+    canvas.create_line(x, y+120, x+95, y+120, width=w, fill=color if highlight else None) # bottom line
+
+def draw_west_hallway(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+20, y+120, x+200, y+120, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+20, y+120, x+20, y+160, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x+20, y+160, x+200, y+160, width=w, fill=color if highlight else None) # bottom line
+    canvas.create_line(x+200, y, x+200, y+120, width=w, fill=color if highlight else None) # left inner line
+    canvas.create_line(x+200, y, x+240, y, width=w, fill=color if highlight else None) # top outer line
+    canvas.create_line(x+240, y, x+240, y+160, width=w, fill=color if highlight else None) # right line
+
+def draw_master_bedroom(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+20, y+160, x+115, y+160, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+20, y+160, x+20, y+280, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x+20, y+280, x+150, y+280, width=w, fill=color if highlight else None) # bottom line
+    canvas.create_line(x+150, y+220, x+150, y+280, width=w, fill=color if highlight else None) # right outer line
+    canvas.create_line(x+115, y+220, x+150, y+220, width=w, fill=color if highlight else None) # bottom inner line
+    canvas.create_line(x+115, y+160, x+115, y+220, width=w, fill=color if highlight else None) # left inner line
+
+def draw_master_bathroom(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+115, y+220, x+180, y+220, width=w, fill=color if highlight else None) # bottom line
+    canvas.create_line(x+115, y+160, x+115, y+220, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x+115, y+160, x+180, y+160, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+180, y+160, x+180, y+220, width=w, fill=color if highlight else None) # right line
+
+def draw_bedroom_2(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+135, y, x+200, y, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+135, y, x+135, y+60, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x+95, y+60, x+135, y+60, width=w, fill=color if highlight else None) # inner top line
+    canvas.create_line(x+95, y+60, x+95, y+120, width=w, fill=color if highlight else None) # left outer line
+    canvas.create_line(x+95, y+120, x+200, y+120, width=w, fill=color if highlight else None) # left outer line
+    canvas.create_line(x+200, y, x+200, y+120, width=w, fill=color if highlight else None) # right line
+
+def draw_main_living_room(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+150, y+280, x+270, y+280, width=w, fill=color if highlight else None) # bottom line
+    canvas.create_line(x+150, y+220, x+150, y+280, width=w, fill=color if highlight else None) # left outer
+    canvas.create_line(x+150, y+220, x+180, y+220, width=w, fill=color if highlight else None) # inner top
+    canvas.create_line(x+180, y+160, x+180, y+220, width=w, fill=color if highlight else None) # left inner
+    canvas.create_line(x+180, y+160, x+270, y+160, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+270, y+160, x+270, y+280, width=w, fill=color if highlight else None) # right line
+
+def draw_kitchen(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+270, y+160, x+270, y+280, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x+270, y+280, x+350, y+280, width=w, fill=color if highlight else None) # bottom line
+    canvas.create_line(x+350, y+115, x+350, y+280, width=w, fill=color if highlight else None) # right line
+    canvas.create_line(x+270, y+160, x+300, y+160, width=w, fill=color if highlight else None) # top inner line
+    canvas.create_line(x+300, y+115, x+300, y+160, width=w, fill=color if highlight else None) # left inner line
+    canvas.create_line(x+300, y+115, x+350, y+115, width=w, fill=color if highlight else None) # top outer line
+
+def draw_dining_room(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+240, y+160, x+300, y+160, width=w, fill=color if highlight else None) # bottom line
+    canvas.create_line(x+300, y+115, x+330, y+115, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+300, y+115, x+300, y+160, width=w, fill=color if highlight else None) # right line
+    canvas.create_line(x+240, y+55, x+240, y+160, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x+330, y+55, x+330, y+115, width=w, fill=color if highlight else None) # right above
+    canvas.create_line(x+240, y+55, x+330, y+55, width=w, fill=color if highlight else None) # top above
+    
+def draw_hallway_bathroom(canvas, x, y, highlight=False, color=COLOR_BLUE):
+    w = 3 if highlight else 1
+    canvas.create_line(x+240, y, x+330, y, width=w, fill=color if highlight else None) # top line
+    canvas.create_line(x+240, y, x+240, y+55, width=w, fill=color if highlight else None) # left line
+    canvas.create_line(x+240, y+55, x+330, y+55, width=w, fill=color if highlight else None) # bottom line
+    canvas.create_line(x+330, y, x+330, y+55, width=w, fill=color if highlight else None) # right line
+
+def draw_home_layout(canvas, active=None, x=20, y=20, color=COLOR_BLUE):
+    canvas.delete('all')
+    draw_bedroom_1(canvas, x, y, active==ROOMS[0], color=color)
+    draw_west_hallway(canvas, x, y, False)
+    draw_master_bedroom(canvas, x, y, active==ROOMS[1], color=color)
+    draw_master_bathroom(canvas, x, y, active==ROOMS[2], color=color)
+    draw_bedroom_2(canvas, x, y, active==ROOMS[3], color=color)
+    draw_main_living_room(canvas, x, y, active==ROOMS[4], color=color)
+    draw_kitchen(canvas, x, y, active==ROOMS[5], color=color)
+    draw_dining_room(canvas, x, y, active==ROOMS[6], color=color)
+    draw_hallway_bathroom(canvas, x, y, active==ROOMS[7], color=color)
+
+###### Tkinter Screen Setup
 
 root = tk.Tk()
-root.title('Introduction to Microcontrollers - Home Control System')
+root.title('Home Control System')
 root.resizable(False, False)
+root.iconbitmap(PROGRAM_LOGO)
 
 '''
 Menus
@@ -65,14 +179,15 @@ menu_bar = tk.Menu(root, tearoff=tk.FALSE)
 options_menu = tk.Menu(menu_bar, tearoff=tk.FALSE)
 ports_submenu = tk.Menu(options_menu, tearoff=tk.FALSE)
 for port,device in list_comports():
-    ports_submenu.add_checkbutton(label=f'{port} - {device}', command=lambda: set_port(port))
+    ports_submenu.add_checkbutton(label=f'{port} - {device}', command=set_port_func(port))
 options_menu.add_cascade(label='Set Port', menu=ports_submenu)
 baud_submenu = tk.Menu(options_menu, tearoff=tk.FALSE)
 for rate in AVAILABLE_BAUD_RATES:
-    baud_submenu.add_checkbutton(label=rate, command=lambda: set_baudrate(rate))
+    baud_submenu.add_checkbutton(label=rate, command=set_baudrate_func(rate))
 options_menu.add_cascade(label='Set Baud Rate', menu=baud_submenu)
 options_menu.add_separator()
 options_menu.add_command(label='How To Use', command=lambda: create_popup('How To Use', 'Set the baud rate and port, then connect to Arduino.\nThe program should automatically update with the arduino program.'))
+options_menu.add_command(label='About', command=lambda: create_popup('About', 'Intro to Microcontrollers - Home Control System, for Arduino Nano Explorer EV58G97A'))
 options_menu.add_command(label='Quit', command=root.destroy)
 menu_bar.add_cascade(label='Options', menu=options_menu)
 menu_bar.add_command(label='Connect to Arduino', command=connect_to_ardunio)
@@ -90,128 +205,52 @@ info_font = font.Font(family='Sans', size=12, weight='normal')
 title_label = ttk.Label(main_frame, text='Home View', font=title_font)
 title_label.grid(row=0, column=0, sticky=(tk.N), padx=10)
 
-canvas = tk.Canvas(main_frame, width=400, height=350, background='#f4f4f4', highlightbackground='#000', highlightcolor='#000', highlightthickness=0)
+canvas = tk.Canvas(main_frame, width=400, height=350, background='#f7f7f7', highlightbackground='#f0f0f0', highlightcolor='#f0f0f0', highlightthickness=1)
 canvas.grid(row=1, column=0, sticky=(tk.N,tk.E,tk.W), pady=10)
 
-def draw_bedroom_1(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x, y, x, y+120, width=w) # left line
-    canvas.create_line(x, y, x+135, y, width=w) # top line
-    canvas.create_line(x+135, y, x+135, y+60, width=w) # right line stick
-    canvas.create_line(x+95, y+60, x+135, y+60,width=w) # bottom line stick
-    canvas.create_line(x+95, y+60, x+95, y+120, width=w) # right line inner
-    canvas.create_line(x, y+120, x+95, y+120, width=w) # bottom line
-
-def draw_west_hallway(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+20, y+120, x+200, y+120, width=w) # top line
-    canvas.create_line(x+20, y+120, x+20, y+160, width=w) # left line
-    canvas.create_line(x+20, y+160, x+200, y+160, width=w) # bottom line
-    canvas.create_line(x+200, y, x+200, y+120, width=w) # left inner line
-    canvas.create_line(x+200, y, x+240, y, width=w) # top outer line
-    canvas.create_line(x+240, y, x+240, y+160, width=w) # right line
-
-def draw_master_bedroom(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+20, y+160, x+115, y+160, width=w) # top line
-    canvas.create_line(x+20, y+160, x+20, y+280, width=w) # left line
-    canvas.create_line(x+20, y+280, x+150, y+280, width=w) # bottom line
-    canvas.create_line(x+150, y+220, x+150, y+280, width=w) # right outer line
-    canvas.create_line(x+115, y+220, x+150, y+220, width=w) # bottom inner line
-    canvas.create_line(x+115, y+160, x+115, y+220, width=w) # left inner line
-
-def draw_master_bathroom(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+115, y+220, x+180, y+220, width=w) # bottom line
-    canvas.create_line(x+115, y+160, x+115, y+220, width=w) # left line
-    canvas.create_line(x+115, y+160, x+180, y+160, width=w) # top line
-    canvas.create_line(x+180, y+160, x+180, y+220, width=w) # right line
-
-def draw_bedroom_2(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+135, y, x+200, y, width=w) # top line
-    canvas.create_line(x+135, y, x+135, y+60, width=w) # left line
-    canvas.create_line(x+95, y+60, x+135, y+60, width=w) # inner top line
-    canvas.create_line(x+95, y+60, x+95, y+120, width=w) # left outer line
-    canvas.create_line(x+95, y+120, x+200, y+120, width=w) # left outer line
-    canvas.create_line(x+200, y, x+200, y+120, width=w) # right line
-
-def draw_main_living_room(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+150, y+280, x+270, y+280, width=w) # bottom line
-    canvas.create_line(x+150, y+220, x+150, y+280, width=w) # left outer
-    canvas.create_line(x+150, y+220, x+180, y+220, width=w) # inner top
-    canvas.create_line(x+180, y+160, x+180, y+220, width=w) # left inner
-    canvas.create_line(x+180, y+160, x+270, y+160, width=w) # top line
-    canvas.create_line(x+270, y+160, x+270, y+280, width=w) # right line
-
-def draw_kitchen(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+270, y+160, x+270, y+280, width=w) # left line
-    canvas.create_line(x+270, y+280, x+350, y+280, width=w) # bottom line
-    canvas.create_line(x+350, y+115, x+350, y+280, width=w) # right line
-    canvas.create_line(x+270, y+160, x+300, y+160, width=w) # top inner line
-    canvas.create_line(x+300, y+115, x+300, y+160, width=w) # left inner line
-    canvas.create_line(x+300, y+115, x+350, y+115, width=w) # top outer line
-
-def draw_dining_room(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+240, y+160, x+300, y+160, width=w) # bottom line
-    canvas.create_line(x+300, y+115, x+330, y+115, width=w) # top line
-    canvas.create_line(x+300, y+115, x+300, y+160, width=w) # right line
-    canvas.create_line(x+240, y+55, x+240, y+160, width=w) # left line
-    canvas.create_line(x+330, y+55, x+330, y+115, width=w) # right above
-    canvas.create_line(x+240, y+55, x+330, y+55, width=w) # top above
-    
-def draw_hallway_bathroom(canvas, x, y, highlight=False):
-    w = 3 if highlight else 1
-    canvas.create_line(x+240, y, x+330, y, width=w) # top line
-    canvas.create_line(x+240, y, x+240, y+55, width=w) # left line
-    canvas.create_line(x+240, y+55, x+330, y+55, width=w) # bottom line
-    canvas.create_line(x+330, y, x+330, y+55, width=w) # right line
-
-def draw_home_layout(canvas, active=None, x=10, y=10):
-    canvas.delete('all')
-    draw_bedroom_1(canvas, x, y, active==ROOMS[0])
-    draw_west_hallway(canvas, x, y, False)
-    draw_master_bedroom(canvas, x, y, active==ROOMS[1])
-    draw_master_bathroom(canvas, x, y, active==ROOMS[2])
-    draw_bedroom_2(canvas, x, y, active==ROOMS[3])
-    draw_main_living_room(canvas, x, y, active==ROOMS[4])
-    draw_kitchen(canvas, x, y, active==ROOMS[5])
-    draw_dining_room(canvas, x, y, active==ROOMS[6])
-    draw_hallway_bathroom(canvas, x, y, active==ROOMS[7])
-
+# labels
 room_var = tk.StringVar(main_frame, 'Selected Room:')
-
+temperature_var = tk.StringVar(main_frame, 'Temperature:')
 room_label = ttk.Label(main_frame, textvariable=room_var, font=info_font)
 room_label.grid(row=2, column=0, sticky=(tk.W), padx=10)
-temp_label = ttk.Label(main_frame, text='Temperature:', font=info_font)
+temp_label = ttk.Label(main_frame, textvariable=temperature_var, font=info_font)
 temp_label.grid(row=3, column=0, sticky=(tk.W), padx=10)
 
-draw_home_layout(canvas, active=ROOMS[ROOM_INDEX], x=20)
-room_var.set(f'Selected Room: {ROOMS[ROOM_INDEX]}')
+# draw canvas and set values
+draw_home_layout(canvas, active=None)
 
-def next_room():
-    global ROOM_INDEX
-    ROOM_INDEX += 1
-    if ROOM_INDEX >= len(ROOMS):
-        ROOM_INDEX = 0
-    draw_home_layout(canvas, active=ROOMS[ROOM_INDEX], x=20)
+def update_temperature(temperature):
+    print(f'Updating with temperature {temperature}C...')
+    global temperature_var
+    temperature_var.set(f'Temperature: {temperature} C')
+
+def update_selected_room(room, color):
+    print(f'Updating room {room} with color {color}!')
+    ROOM_INDEX = room
+    global canvas
+    if color == 'red':
+        draw_home_layout(canvas, active=ROOMS[ROOM_INDEX], color=COLOR_RED)
+    elif color == 'green':
+        draw_home_layout(canvas, active=ROOMS[ROOM_INDEX], color=COLOR_GREEN)
+    else:
+        draw_home_layout(canvas, active=ROOMS[ROOM_INDEX], color=COLOR_BLUE)
     room_var.set(f'Selected Room: {ROOMS[ROOM_INDEX]}')
 
-test_button = ttk.Button(main_frame, text='switch', command=next_room)
-test_button.grid(row=4, column=0, sticky=(tk.W), padx=10)
-
-
-def update_selected_room(room_num):
-    room_label.config(text=f'Selected Room: {room_num}')
-
-def update_temperature(temp):
-    temp_label.config(text=f'Temperature: {temp}')
+def arduino_update_ui(text_input):
+    # expecting: "temperature: 34C"
+    if text_input.startswith('temperature:'):
+        update_temperature(text_input[text_input.index(':')+1:].strip())
+    # expecting: "room: 3 color: blue"
+    elif text_input.startswith('room:'):
+        color_index = text_input.index('color:')
+        room = int(text_input[5:color_index].strip())
+        color = text_input[color_index+6:].strip()
+        update_selected_room(room, color)
 
 def create_popup(title, text, width=300, height=100):
+    global root
     popup = tk.Toplevel(root, padx=10, pady=10, width=width, height=height)
+    popup.iconbitmap(PROGRAM_LOGO)
     x = root.winfo_x() + (root.winfo_width() - width) // 2
     y = root.winfo_y() + (root.winfo_height() - height) // 2
     popup.geometry(f'+{x}+{y}')
--- deliverables/5_lab-project/lab/code/UART_To_Python/uart_send_data/uart_send_data.ino
+++ deliverables/5_lab-project/lab/code/UART_To_Python/uart_send_data/uart_send_data.ino
@@ -5,12 +5,14 @@ void setup() {
 }
 
 void loop() {
-  static uint8_t count = 1;
-  
-  Serial.printf("%d\n", count);
-  count++;
-  if (count > 10) {
-    count = 1;
+  for (int i = 0; i < 8; i++) {
+    Serial.printf("Temperature: %d\n", i);
+    if (i % 5 == 0)
+      Serial.printf("Room: %d Color: Green\n", i);
+    else if (i % 3 == 0)
+      Serial.printf("Room: %d Color: Blue\n", i);
+    else
+      Serial.printf("Room: %d Color: Red\n", i);
+    delay(2000);
   }
-  delay(1000);
 }
