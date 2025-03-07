d81316a08b47edfe312e364355827b148a8235a3
resire02 <howfelix@gmail.com>
2025-03-06 23:48:16 -0700

    cleaned app code, introduces graphing bug

--- deliverables/9_lab-2-mplabx/Prototype/adc_hist.py
+++ deliverables/9_lab-2-mplabx/Prototype/adc_hist.py
@@ -1,4 +1,5 @@
-from cProfile import label
+import itertools
+from multiprocessing import Value
 import tkinter as tk
 import serial
 import serial.tools.list_ports_windows as list_ports
@@ -8,57 +9,163 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import seaborn as sns
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import threading
+import logging
 
-incoming_data = []
+APP_NAME = 'ADC Live Plotter'
+logger = logging.getLogger(APP_NAME)
+logging.basicConfig(filename='adc.log', level=logging.INFO)
 
-def read_adc_thread() -> None:
-    # print('Available Ports:', ''.join([x.name for x in list_ports.comports()]))
-    uart = serial.Serial(port='COM6', baudrate=115200, timeout=0.1)
-    while True:
-        if uart.in_waiting:
-            uart_incoming_data = uart.readline().decode('utf-8').replace('\n', '')
-            uart_match = re.findall(r'chan \d{1} (\d+)', uart_incoming_data)
-            if uart_match is not None:
-                channel_reads = [int(x) for x in uart_match]
-                incoming_data.append(channel_reads[0])
+class UARTDataReader:
+    def __init__(self):
+        self._pattern = r'chan \d{1} (\d+)'
+        self._baudrate = None
+        self._port = None
+        self.channel_1 = []
+        self.channel_2 = []
+        self._uart_thread = None
+        self._is_connected = False
+
+    def set_port(self, port):
+        if not self._is_connected:
+            self._port = port
+            return True
+        return False
+
+    def set_baudrate(self, rate):
+        if not self._is_connected:
+            self._baudrate = rate
+            return True
+        return False
+
+    def set_pattern(self, pattern):
+        if not self._is_connected:
+            self._pattern = pattern
+            return True
+        return False
+
+    def read_adc_thread(self):
+        try:
+            logger.info(f'Attempting connection on {self._port} with a {self._baudrate} rate.')
+            if self._pattern is None or self._baudrate is None or self._port is None:
+                raise ValueError()
+            with serial.Serial(port=self._port, baudrate=self._baudrate, timeout=0.1) as uart:
+                self._is_connected = True
+                while self._is_connected:
+                    if uart.in_waiting:
+                        uart_incoming_data = uart.readline().decode('utf-8').replace('\n', '')
+                        uart_match = re.findall(self._pattern, uart_incoming_data)
+                        if uart_match is not None:
+                            channel_reads = [int(x) for x in uart_match[:2]]
+                            logger.info(f'Received data: {channel_reads}')
+                            if len(channel_reads) >= 2:
+                                print(channel_reads)
+                                self.channel_1.append(channel_reads[0])
+                                self.channel_2.append(channel_reads[1])
+                            else:
+                                logger.warning('Unable to parse channel readings, are you using the right baudrate?')
+        except serial.SerialException as e:
+            logger.error(f'Failed to open connection to {self._port}: {e}')
+        except ValueError:
+            logger.error('Failed to open serial connection: Did you set baudrate and port?')
+        finally:
+            logger.info('Exiting ADC data thread...')
+            self._is_connected = False
+
+    def start(self):
+        self._uart_thread = threading.Thread(target=self.read_adc_thread, daemon=True)
+        self._uart_thread.start()
+
+    def is_connected(self):
+        return self._is_connected
 
 
-def gui_thread() -> None:
-    root = tk.Tk()
-    root.title('ADC Live Plotter')
-    root.option_add('*tearOff', tk.FALSE)
-    root.geometry('500x500')
-
-    menubar = tk.Menu(root)
-    options_menu = tk.Menu(menubar)
-    port_options = tk.Menu(options_menu)
-    for port in [x.name for x in list_ports.comports()]:
-        port_options.add_checkbutton(label=port, command=lambda: print(port))
-    options_menu.add_cascade(label='Select Port', menu=port_options)
-    rate_options = tk.Menu(options_menu)
-    rate_options.add_cascade(label='9600', command=lambda: print(9600))
-    rate_options.add_cascade(label='115200', command=lambda: print(115200))
-    options_menu.add_cascade(label='Select Baudrate', menu=rate_options)
-    menubar.add_cascade(menu=options_menu, label='Options')
-    root.config(menu=menubar)
-
-    app = tk.Frame(root)
-    chart = Figure(figsize=(5,5), dpi=100)
-    axes = chart.add_subplot()
-    def animate(frame_count, data):
-        axes.clear()
-        sns.histplot(data=data, kde=False, ax=axes, color='gray')
-    canvas = FigureCanvasTkAgg(figure=chart, master=root)
-    canvas.get_tk_widget().pack()
-    hold = animation.FuncAnimation(chart, func=animate, fargs=[incoming_data], frames=1, interval=100)
+class ADCApplication:
+    def __init__(self):
+        self.uart_reader = UARTDataReader()
+        self.root = tk.Tk()
+        self._BAUDRATES = [9600, 115200]
+        self._selected_port = None
+        self._selected_rate = None
+        self._init_gui()
+        self._refresh_menus()
     
-    app.pack()
-    root.mainloop()
+    def _refresh_menus(self):
+        logger.info('Refreshing ports and options menus...')
+        self.port_options.delete(0, self.port_options.index('end'))
+        for port in [x.name for x in list_ports.comports()]:
+            self.port_options.add_checkbutton(label=port, command=lambda port=port: self._set_port(port))
+        self.rate_options.delete(0, self.rate_options.index('end'))
+        for rate in self._BAUDRATES:
+            self.rate_options.add_cascade(label=str(rate), command=lambda rate=rate: self._set_rate(rate))
+
+    def _set_port(self, port):
+        if self.uart_reader.set_port(port):
+            logger.info(f'Set port: {port}')
+            self.options_menu.entryconfig(0, label=f'Port: {port}')
+        else:
+            logger.warning('Cannot set port, please stop the live graphing first')
+
+    def _set_rate(self, rate):
+        if self.uart_reader.set_baudrate(rate):
+            logger.info(f'Set baudrate: {rate}')
+            self.options_menu.entryconfig(1, label=f'Baudrate: {rate}')
+        else:
+            logger.warning('Cannot set baudrate, please stop the live graphing first')
+    
+    def _init_gui(self):
+        # Root config
+        self.root.title(APP_NAME)
+        self.root.option_add('*tearOff', tk.FALSE)
+        # self.root.geometry('500x500')
+        # Menubar setup
+        menubar = tk.Menu(self.root)
+        self.options_menu = tk.Menu(menubar)
+        self.port_options = tk.Menu(self.options_menu)
+        self.rate_options = tk.Menu(self.options_menu)
+        self.options_menu.add_cascade(label='Port', menu=self.port_options)
+        self.options_menu.add_cascade(label='Baudrate', menu=self.rate_options)
+        self.options_menu.add_command(label='Refresh Ports', command=self._refresh_menus)
+        menubar.add_cascade(menu=self.options_menu, label='Options')
+        menubar.add_command(label='Start', command=self._run_uart)
+        menubar.add_command(label='Stop', state='disabled', command=lambda: print('Stop'))
+        self.root.config(menu=menubar)
+        # App frame setup
+        app = tk.Frame(self.root)
+        # TK Graph setup
+        self.chart1 = Figure(figsize=(5,5), dpi=100)
+        self.g1_axes = self.chart1.add_subplot()
+        self.graph_1_animation = animation.FuncAnimation(self.chart1, func=self.graph_1_animation_func, fargs=[self.uart_reader.channel_1], frames=1, blit=False, interval=100, repeat=True)
+        self.chart2 = Figure(figsize=(5,5), dpi=100)
+        self.g2_axes = self.chart2.add_subplot()
+        self.graph_2_animation = animation.FuncAnimation(self.chart2, func=self.graph_2_animation_func, fargs=[self.uart_reader.channel_2], frames=1, blit=False, interval=100, repeat=True)
+        self.canvas1 = FigureCanvasTkAgg(figure=self.chart1, master=app)
+        self.canvas2 = FigureCanvasTkAgg(figure=self.chart2, master=app)
+        self.canvas1.get_tk_widget().pack()
+        self.canvas2.get_tk_widget().pack()
+        app.pack()
+
+    def graph_1_animation_func(self, frame_index, data):
+        print('Got:', data)
+        self.g1_axes.clear()
+        sns.histplot(data=data, kde=False, ax=self.g1_axes, color='gray')
+
+    def graph_2_animation_func(self, frame_index, data):
+        self.g2_axes.clear()
+        sns.histplot(data=data, kde=False, ax=self.g2_axes, color='gray')
+
+    def _run_uart(self):
+        if not self.uart_reader.is_connected():
+            self.uart_reader.start()
+        else:
+            logger.error('Unable to start UART process')
+
+    def run(self):
+        self.root.mainloop()
 
 
 if __name__ == "__main__":
-    adc_thread = threading.Thread(target=read_adc_thread, daemon=True)
-    adc_thread.start()
-    gui_thread()
+    app = ADCApplication()
+    app.run()
