bd767cff56736c0d8f518837e340b7513bf8badd
resire02 <howfelix@gmail.com>
2025-04-06 16:12:21 -0700

    US236 T286: adding working project files for lab 2 and consistent-ized lab name

---
+++ deliverables/09_lab-2-mplabx/lab/code/lab2_adc_differential.X/mcc_generated_files/adc/adc0.h
@@ -0,0 +1,313 @@
+/**
+ * ADC0 Generated Driver API Header File
+ * 
+ * @file adc0.h
+ * 
+ * @defgroup  adc0 Analog-to-Digital Converter (ADC0)
+ * 
+ * @brief This file contains the API prototypes and data types for the ADC0 driver.
+ *
+ * @version ADC0 Driver Version 2.0.0
+ * 
+ * @version ADC0 Package Version 5.0.0
+*/
+
+/*
+© [2025] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+
+#ifndef ADC0_H
+#define ADC0_H
+
+#include "../system/utils/compiler.h"
+#include "./adc_types.h"
+
+/**
+ * @ingroup adc0
+ * @brief Defines the Custom Name pin mapping for channels in @ref adc_channel_t
+ */
+#define IO_PD6 ADC0_CHANNEL_AIN6
+
+/**
+ * @ingroup adc0
+ * @brief Defines the Custom Name pin mapping for channels in @ref adc_channel_t
+ */
+#define IO_PD7 ADC0_CHANNEL_AIN7
+
+/**
+ * @ingroup adc0
+ * @brief Defines the Custom Name pin mapping for channels in @ref adc_channel_t
+ */
+#define IO_PD1 ADC0_CHANNEL_AIN1
+
+/**
+ * @ingroup adc0
+ * @brief Initializes the registers based on the configurable options in the MPLABÂ® Code Configurator (MCC) Melody UI for the Analog-to-Digital Converter (ADC) operation.
+ * @param None.
+ * @return None.
+ */
+void ADC0_Initialize(void);
+
+/**
+ * @ingroup adc0
+ * @brief Deinitializes the registers to Power-on Reset values.
+ * @param None.
+ * @return None.
+ */
+void ADC0_Deinitialize(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the ADC Enable bit to `1`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_Enable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the ADC Enable bit to `0`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_Disable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the channel to use for the ADC conversion.
+ * @param channel Desired analog channel. Refer to the @ref adc_channel_t enum for the list of available analog channels.
+ * @return None.
+ */
+void ADC0_ChannelSelect(adc_channel_t channel);
+
+/**
+ * @ingroup adc0
+ * @brief Starts the ADC conversion on a selected channel. 
+ * @pre Select the channel using @ref ADC0_ChannelSelect and 
+ * call @ref ADC0_Initialize(void) to initialize the ADC module before using this API.
+ * @return None.
+ */
+void ADC0_ConversionStart(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Result Ready Interrupt Enable (RESRDY) bit to `1`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ConversionDoneInterruptEnable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Result Ready Interrupt Enable (RESRDY) bit to `0`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ConversionDoneInterruptDisable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Checks if the ongoing ADC conversion is complete.
+ * @param None.
+ * @retval True - The conversion is complete
+ * @retval False - The conversion is ongoing
+ */
+bool ADC0_IsConversionDone(void);
+
+/**
+ * @ingroup adc0
+ * @brief Retrieves the result of the latest conversion.
+ * @param None.
+ * @return adc_result_t - The result of the conversion
+ */
+adc_result_t ADC0_ConversionResultGet(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the callback function for the Result Ready Interrupt (RESRDY).
+ * @param *callback The pointer to the function to be executed
+ * @return None.
+ */
+void ADC0_ConversionDoneCallbackRegister(void (*callback)(void));
+
+/**
+ * @ingroup adc0
+ * @brief Starts the conversion and retrieves the result of one conversion on the selected channel. 
+ * @param channel Desired analog channel. Refer to the @ref adc_channel_t enum for the list of available analog channels.
+ * @return adc_result_t - The result of the conversion
+ */
+adc_result_t ADC0_ChannelSelectAndConvert(adc_channel_t channel);
+
+/**
+ * @ingroup adc0
+ * @brief Returns the resolution of the ADC module.
+ * @param None.
+ * @return uint8_t - Resolution value
+ */
+uint8_t ADC0_ResolutionGet(void);
+
+/**
+ * @ingroup adc0
+ * @brief Stops the ongoing ADC conversion.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ConversionStop(void);
+
+/**
+ * @ingroup adc0
+ * @brief Loads the Sample Length with the specified value.
+ * @param repeatCount Repeat count value. Refer to the @ref adc_repeat_count_t enum for the list of available repeat count values.
+ * @return None.
+ */
+void ADC0_SampleRepeatCountSet(adc_repeat_count_t repeatCount);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Computation mode. 
+ * @param computationMode Desired computation mode. Refer to the @ref adc_computation_mode_t enum for the list of available computation modes.
+ * @return None.
+ */
+void ADC0_ComputationModeSet(adc_computation_mode_t computationMode);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the value of the Window Comparator High Threshold (WINHT) register.
+ * @param upperThreshold Upper threshold value of the @ref adc_threshold_t type
+ * @return None.
+ */
+void ADC0_UpperThresholdSet(adc_threshold_t upperThreshold);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the value of the Window Comparator Low Threshold (WINLT) register.
+ * @param lowerThreshold Lower threshold value of the @ref adc_threshold_t type
+ * @return None.
+ */
+void ADC0_LowerThresholdSet(adc_threshold_t upperThreshold);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Window Comparator mode.
+ * @param thresholdMode Desired threshold mode. Refer to the @ref adc_threshold_mode_t enum for the list of available threshold modes.
+ * @return None.
+ */
+void ADC0_ThresholdModeSet(adc_threshold_mode_t thresholdMode);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Window Comparator Interrupt Enable (WCMP) bit to `1`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ThresholdInterruptEnable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Window Comparator Interrupt Enable (WCMP) bit to `0`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ThresholdInterruptDisable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Retrieves the value of the accumulated conversions.
+ * @param None.
+ * @return adc_accumulate_t - The value of accumulated conversions
+ */
+adc_accumulate_t ADC0_AccumulatedResultGet(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the callback function for the Window Comparator Interrupt (WCMP).
+ * @param *callback The pointer to the function to be executed
+ * @return None.
+ */
+void ADC0_ThresholdCallbackRegister(void (*callback)(void));
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Free-Running (FREERUN) bit to `1`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ContinuousConversionEnable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Free-Running (FREERUN) bit to `0`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ContinuousConversionDisable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Start Event Input (STARTEI) bit to `1`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_AutoTriggerEnable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Sets the Start Event Input (STARTEI) bit to `0`.
+ * @param None.
+ * @return None.
+ */
+void ADC0_AutoTriggerDisable(void);
+
+/**
+ * @ingroup adc0
+ * @brief Checks the Result Ready Interrupt (RESRDY) flag status.
+ * @param None.
+ * @retval True - RESRDY flag is set
+ * @retval False - RESRDY flag is not set
+ */
+bool ADC0_IsConversionDoneInterruptFlagSet(void);
+
+/**
+ * @ingroup adc0
+ * @brief Clears the Result Ready Interrupt (RESRDY) flag.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ConversionDoneInterruptFlagClear(void);
+
+/**
+ * @ingroup adc0
+ * @brief Checks the Window Comparator Interrupt (WCMP) flag status.
+ * @param None.
+ * @retval True - WCMP flag is set
+ * @retval False - WCMP flag is not set
+ */
+bool ADC0_IsThresholdInterruptFlagSet(void);
+
+/**
+ * @ingroup adc0
+ * @brief Clears the Window Comparator Interrupt (WCMP) flag.
+ * @param None.
+ * @return None.
+ */
+void ADC0_ThresholdInterruptFlagClear(void);
+
+#endif /* ADC0_H */
---
+++ deliverables/09_lab-2-mplabx/lab/code/lab2_adc_oversampling.X/mcc_generated_files/adc/src/adc0.c
@@ -0,0 +1,301 @@
+/**
+ * ADC0 Generated Driver File
+ *
+ * @file adc0.c
+ * 
+ * @ingroup adc0 
+ * 
+ * @brief This file contains the API implementations for the ADC0 driver.
+ * 
+ * @version ADC0 Driver Version 2.0.0
+ * 
+ * @version ADC0 Package Version 5.0.0
+*/
+
+/*
+© [2025] Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip 
+    software and any derivatives exclusively with Microchip products. 
+    You are responsible for complying with 3rd party license terms  
+    applicable to your use of 3rd party software (including open source  
+    software) that may accompany Microchip software. SOFTWARE IS ?AS IS.? 
+    NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS 
+    SOFTWARE, INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT,  
+    MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT 
+    WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY 
+    KIND WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF 
+    MICROCHIP HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE 
+    FORESEEABLE. TO THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP?S 
+    TOTAL LIABILITY ON ALL CLAIMS RELATED TO THE SOFTWARE WILL NOT 
+    EXCEED AMOUNT OF FEES, IF ANY, YOU PAID DIRECTLY TO MICROCHIP FOR 
+    THIS SOFTWARE.
+*/
+
+#include "../adc0.h"
+
+static void (*ADC0_ConversionDoneCallback)(void);
+static void (*ADC0_ThresholdCallback)(void);
+
+void ADC0_Initialize(void)
+{
+    // Disable ADC0 module
+    ADC0.CTRLA &= ~(ADC_ENABLE_bm);
+    // Disable ADC0 interrupts
+    ADC0.INTCTRL &= ~(ADC_RESRDY_bm | ADC_WCMP_bm);
+    // Clear ADC0 interrupt flags
+    ADC0.INTFLAGS = (ADC_RESRDY_bm | ADC_WCMP_bm);
+    // Reset interrupt callback handlers
+    ADC0_ConversionDoneCallback = NULL;
+    ADC0_ThresholdCallback = NULL;
+
+    // Configure ADC0 module registers
+    ADC0.CTRLB = (ADC_SAMPNUM_NONE_gc);             /* SAMPNUM (No accumulation) */
+    ADC0.CTRLC = (ADC_PRESC_DIV2_gc);               /* PRESC (CLK_PER divided by 2) */
+    ADC0.CTRLD = (ADC_INITDLY_DLY0_gc)              /* INITDLY (DLY0) */
+				|(ADC_SAMPDLY_DLY0_gc);             /* SAMPDLY (DLY0) */
+    ADC0.CTRLE = (ADC_WINCM_NONE_gc);               /* WINCM (No Window Comparison) */
+    ADC0.SAMPCTRL = 0x0;                               /* SAMPLEN (0x0) */
+    ADC0.MUXPOS = (ADC_MUXPOS_AIN1_gc);              /* MUXPOS (AIN1) */
+    ADC0.MUXNEG = (ADC_MUXNEG_GND_gc);               /* MUXNEG (GND) */
+    ADC0.COMMAND = (0 << ADC_STCONV_bp)               /* STCONV (disabled) */
+				|(0 << ADC_SPCONV_bp);              /* SPCONV (disabled) */
+    ADC0.EVCTRL = (0 << ADC_STARTEI_bp);             /* STARTEI (disabled) */
+    ADC0.DBGCTRL = (0 << ADC_DBGRUN_bp);              /* DBGRUN (disabled) */
+    ADC0.WINHT = 0x0;                               /* WINHT (0x0) */
+    ADC0.WINLT = 0x0;                               /* WINLT (0x0) */
+    ADC0.INTCTRL = (0 << ADC_RESRDY_bp)               /* RESRDY (disabled) */
+				|(0 << ADC_WCMP_bp);                /* WCMP (disabled) */
+
+    ADC0.CTRLA = (1 << ADC_ENABLE_bp)               /* ENABLE (enabled) */
+				|(0 << ADC_FREERUN_bp)              /* FREERUN (disabled) */
+				|(ADC_RESSEL_12BIT_gc)              /* RESSEL (12-bit mode) */
+				|(0 << ADC_RUNSTBY_bp)              /* RUNSTBY (disabled) */
+				|(ADC_CONVMODE_SINGLEENDED_gc)      /* CONVMODE (SINGLEENDED) */
+				|(0 << ADC_LEFTADJ_bp);             /* LEFTADJ (disabled) */
+}
+
+void ADC0_Deinitialize(void)
+{
+    ADC0.CTRLA = 0x0;
+    ADC0.INTCTRL = 0x0;
+    ADC0.INTFLAGS = 0x0;
+    ADC0.CTRLB = 0x0;
+    ADC0.CTRLC = 0x0;
+    ADC0.CTRLD = 0x0;
+    ADC0.CTRLE = 0x0;
+    ADC0.SAMPCTRL = 0x0;
+    ADC0.MUXPOS = 0x0;
+    ADC0.MUXNEG = 0x0;
+    ADC0.COMMAND = 0x0;
+    ADC0.EVCTRL = 0x0;
+    ADC0.DBGCTRL = 0x0;
+    ADC0.WINHT = 0x0;
+    ADC0.WINLT = 0x0;
+}
+
+void ADC0_Enable(void)
+{
+    ADC0.CTRLA |= ADC_ENABLE_bm;
+}
+
+void ADC0_Disable(void)
+{
+    ADC0.CTRLA &= ~ADC_ENABLE_bm;
+}
+
+void ADC0_ChannelSelect(adc_channel_t channel)
+{
+    ADC0.MUXPOS = channel;
+}
+
+void ADC0_ConversionStart(void)
+{
+    ADC0.COMMAND |= ADC_STCONV_bm;
+}
+
+void ADC0_ConversionDoneInterruptEnable(void)
+{
+    ADC0.INTCTRL |= ADC_RESRDY_bm;
+}
+
+void ADC0_ConversionDoneInterruptDisable(void)
+{
+    ADC0.INTCTRL &= ~ADC_RESRDY_bm;
+}
+
+bool ADC0_IsConversionDone(void)
+{
+    return (bool)(!(ADC0.COMMAND & ADC_STCONV_bm));
+}
+
+adc_result_t ADC0_ConversionResultGet(void)
+{
+    return (adc_result_t)(ADC0.RES);
+}
+
+void ADC0_ConversionDoneCallbackRegister(void (*callback)(void))
+{
+    ADC0_ConversionDoneCallback = callback;
+}
+
+adc_result_t ADC0_ChannelSelectAndConvert(adc_channel_t channel)
+{
+    ADC0.MUXPOS = channel;
+    ADC0.COMMAND |= ADC_STCONV_bm;
+
+    while (0U != (ADC0.COMMAND & ADC_STCONV_bm))
+    {
+        // Wait for conversion to finish
+    }
+
+    return (adc_result_t)(ADC0.RES);
+}
+
+uint8_t ADC0_ResolutionGet(void)
+{
+    return (ADC0.CTRLA & ADC_RESSEL_10BIT_gc) ? 10U : 12U;
+}
+
+void ADC0_ConversionStop(void)
+{
+    ADC0.COMMAND |= ADC_SPCONV_bm;
+}
+
+void ADC0_SampleRepeatCountSet(adc_repeat_count_t repeatCount)
+{
+    ADC0.CTRLB = (ADC_SAMPNUM_gm & repeatCount);
+}
+
+void ADC0_ComputationModeSet(adc_computation_mode_t computationMode)
+{
+    if (ADC_BASIC == computationMode)
+    {
+        ADC0.CTRLB = ADC_NO_ACCUMULATION;
+    }
+    else
+    {
+        if (ADC_NO_ACCUMULATION == (ADC0.CTRLB & ADC_SAMPNUM_gm))
+        {
+            ADC0.CTRLB = ADC_2_SAMPLES_ACCUMULATED;
+        }
+        else
+        {
+            // Do nothing
+        }
+    }
+}
+
+void ADC0_UpperThresholdSet(adc_threshold_t upperThreshold)
+{
+    ADC0.WINHT = upperThreshold;
+}
+
+void ADC0_LowerThresholdSet(adc_threshold_t lowerThreshold)
+{
+    ADC0.WINLT = lowerThreshold;
+}
+
+void ADC0_ThresholdModeSet(adc_threshold_mode_t thresholdMode)
+{
+    ADC0.CTRLE = thresholdMode;
+}
+
+void ADC0_ThresholdInterruptEnable(void)
+{
+    ADC0.INTCTRL |= ADC_WCMP_bm;
+}
+
+void ADC0_ThresholdInterruptDisable(void)
+{
+    ADC0.INTCTRL &= ~ADC_WCMP_bm;
+}
+
+adc_accumulate_t ADC0_AccumulatedResultGet(void)
+{
+    return (adc_accumulate_t)(ADC0.RES);
+}
+
+void ADC0_ThresholdCallbackRegister(void (*callback)(void))
+{
+    ADC0_ThresholdCallback = callback;
+}
+
+void ADC0_ContinuousConversionEnable(void)
+{
+    ADC0.CTRLA |= ADC_FREERUN_bm;
+}
+
+void ADC0_ContinuousConversionDisable(void)
+{
+    ADC0.CTRLA &= ~ADC_FREERUN_bm;
+}
+
+void ADC0_AutoTriggerEnable(void)
+{
+    ADC0.EVCTRL |= ADC_STARTEI_bm;
+}
+
+void ADC0_AutoTriggerDisable(void)
+{
+    ADC0.EVCTRL &= ~ADC_STARTEI_bm;
+}
+
+bool ADC0_IsConversionDoneInterruptFlagSet(void)
+{
+    return (bool)(ADC0.INTFLAGS & ADC_RESRDY_bm);
+}
+
+void ADC0_ConversionDoneInterruptFlagClear(void)
+{
+    ADC0.INTFLAGS = ADC_RESRDY_bm;
+}
+
+bool ADC0_IsThresholdInterruptFlagSet(void)
+{
+    return (bool)(ADC0.INTFLAGS & ADC_WCMP_bm);
+}
+
+void ADC0_ThresholdInterruptFlagClear(void)
+{
+    ADC0.INTFLAGS = ADC_WCMP_bm;
+}
+
+/* cppcheck-suppress misra-c2012-2.7 */
+/* cppcheck-suppress misra-c2012-8.2 */
+/* cppcheck-suppress misra-c2012-8.4 */
+/* cppcheck-suppress misra-c2012-8.6 */
+ISR(ADC0_RESRDY_vect)
+{
+    // Clear result ready interrupt flag
+    ADC0.INTFLAGS = ADC_RESRDY_bm;
+
+    if (NULL != ADC0_ConversionDoneCallback)
+    {
+        ADC0_ConversionDoneCallback();
+    }
+    else
+    {
+        // Do nothing
+    }
+}
+
+/* cppcheck-suppress misra-c2012-2.7 */
+/* cppcheck-suppress misra-c2012-8.2 */
+/* cppcheck-suppress misra-c2012-8.4 */
+/* cppcheck-suppress misra-c2012-8.6 */
+ISR(ADC0_WCMP_vect)
+{
+    // Clear window comparator interrupt flag
+    ADC0.INTFLAGS = ADC_WCMP_bm;
+    
+    if (NULL != ADC0_ThresholdCallback)
+    {
+        ADC0_ThresholdCallback();
+    }
+    else
+    {
+        // Do nothing
+    }
+}
