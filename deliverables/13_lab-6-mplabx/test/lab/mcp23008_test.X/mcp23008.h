9937a71436c11586babe742af05e94b0bc85c1de
resire02 <howfelix@gmail.com>
2025-04-27 23:22:08 -0700

    upload mcp23008_test.X

---
+++ deliverables/13_lab-6-mplabx/test/lab/mcp23008_test.X/mcp23008.h
@@ -0,0 +1,46 @@
+/* 
+ * File:   mcp23008.h
+ * Author: 
+ *
+ * Created on April 27, 2025, 3:07 PM
+ */
+
+#ifndef MCP23008_H
+#define	MCP23008_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+/* initializes the MCP23008 IO Expander LEDs */
+void mcp23008_init(void);
+
+/* sets the leds on/off */
+void mcp23008_write_leds(unsigned char state);
+
+/* reads joystick values */
+void mcp23008_read_joystick(void);
+
+/* returns 1 if joystick is up, returns 0 if joystick is not up or timeout period has not expired */
+bool mcp23008_is_joystick_up(void);
+
+/* returns 1 if joystick is down, returns 0 if joystick is not down or timeout period has not expired */
+bool mcp23008_is_joystick_down(void);
+
+/* returns 1 if joystick is left, returns 0 if joystick is not left or timeout period has not expired */
+bool mcp23008_is_joystick_left(void);
+
+/* returns 1 if joystick is right, returns 0 if joystick is not right or timeout period has not expired */
+bool mcp23008_is_joystick_right(void);
+
+/* returns 1 if joystick is pressed, returns 0 if joystick is not pressesd or timeout period has not expired */
+bool mcp23008_is_joystick_pressed(void);
+
+extern volatile unsigned char joystick_debug;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MCP23008_H */
+
